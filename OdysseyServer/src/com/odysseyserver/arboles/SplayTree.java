999d7f85b3defb41710f31504f76de7f9168b8e6
ch99AM <christianalpizar99@gmail.com>
2018-05-20 04:56:48 -0600

    Se trabajo sobre el arbol Splay, implementa casi todas la funcionalidades, falta la de buscar. ArbolAVl permite buscar

--- OdysseyServer/src/com/odysseyserver/arboles/SplayTree.java
+++ OdysseyServer/src/com/odysseyserver/arboles/SplayTree.java
@@ -1,128 +1,165 @@
 package com.odysseyserver.arboles;
 
+import com.odysseyserver.listas.SimpleList;
+import com.odysseyserver.listas.SimpleNode;
+
 /**
- * An implementation of splay trees.
- * <p>
- * Uses the element type's "natural ordering" to compare elements.
- * <p>
- * The code is optimised for clarity rather than efficiency.
+ * 
+ * @param <String>
  */
+public class SplayTree<String extends Comparable<String>> {
 
-public class SplayTree<String extends Comparable<String>, Value> {
+	private SplayNode root; 
 
-	private SplayNode root; // root of the BST
-
-	// BST helper node data type
+	/**
+	 * 
+	 *
+	 */
 	private class SplayNode {
-		private String key; // key
-		private SplayNode left, right; // left and right subtrees
+		private String clave; 
+		private SimpleList<Integer> arrayIndx = new SimpleList<>();
+ 		private SplayNode left, right;
 
-		public SplayNode(String key) {
-			this.key = key;
+		public SplayNode(String clave, Integer indx) {
+			this.clave = clave; this.left = null; this.right = null;
+			addArrayIndx(indx);
+		}
+		public void remArrayIndx(Integer indx) {
+			this.arrayIndx.remove(indx);
+		}
+		public void addArrayIndx(Integer indx){
+			this.arrayIndx.add(new SimpleNode<Integer>(indx));
 		}
 	}
-
-	public boolean contains(String key) {
-		return get(key) != null;
+	/**
+	 * Comprueba la existencia que un nodo en el arbol
+	 * 
+	 * @param clave
+	 * 				Clave que sera buscada
+	 * @return
+	 * 			true si la clave esta contenida en el arbol/ false de lo contrario
+	 */
+	public boolean contains(String clave) {
+		return get(clave) != null;
 	}
-	
+
 	public void preorder(SplayNode r) {
 		if (r != null) {
-			System.out.println(r.key);
+			System.out.println(r.clave);
 			preorder(r.left);
 			preorder(r.right);
 		}
 	}
-	
+	/**
+	 * Permite obtener la raiz 
+	 * @return
+	 */
 	public SplayNode getRoot() {
 		return this.root;
 	}
 
-	// return value associated with the given key
-	// if no such value, return null
-	public String get(String key) {
-		root = splay(root, key);
-		int cmp = key.compareTo(root.key);
+	/**
+	 * Realiza la busqueda comcreta de un nodo indicando su nombre 
+	 *  @param clave
+	 *  			Nombre del nodo a buscar 
+	 * @return
+	 * 			El nombre del nodo en caso de encontrarlo
+	 * 			De lo contrario null
+	 */
+	public String get(String clave) {
+		root = splay(root, clave);
+		int cmp = clave.compareTo(root.clave);
 		if (cmp == 0) {
-			return key;
-		}
-		else {
+			return clave;
+		} else {
 			return null;
 		}
 	}
 
-	/***************************************************************************
-	 * Splay tree insertion.
-	 ***************************************************************************/
-	public void put(String key) {
-		// splay key to root
+	/**
+	 * Inserta un nodo en el arbol
+	 * @param clave
+	 * 				Nombre del nodo 
+	 * 
+	 * @param indx
+	 * 			Indice donde se puede encontar el archivo con el nombre del 
+	 * 			nodo
+	 * 			
+	 */
+	public void insert(String clave, Integer indx) {
 		if (root == null) {
-			root = new SplayNode(key);
+			root = new SplayNode(clave, indx);
 			return;
 		}
+		root = splay(root, clave);
+		int cmp = clave.compareTo(root.clave);
 
-		root = splay(root, key);
-
-		int cmp = key.compareTo(root.key);
-
-		// Insert new node at root
 		if (cmp < 0) {
-			SplayNode n = new SplayNode(key);
+			SplayNode n = new SplayNode(clave, indx);
 			n.left = root.left;
 			n.right = root;
 			root.left = null;
 			root = n;
 		}
-
-		// Insert new node at root
 		else if (cmp > 0) {
-			SplayNode n = new SplayNode(key);
+			SplayNode n = new SplayNode(clave, indx);
 			n.right = root.right;
 			n.left = root;
 			root.right = null;
 			root = n;
 		}
-
-		// It was a duplicate key. Simply replace the value
 		else {
-			root.key = key;
+			root.clave = clave;
+			root.addArrayIndx(indx);
 		}
 
 	}
 
-	/***************************************************************************
-	 * Splay tree deletion.
-	 ***************************************************************************/
-	/*
-	 * This splays the key, then does a slightly modified Hibbard deletion on the
-	 * root (if it is the node to be deleted; if it is not, the key was not in the
-	 * tree). The modification is that rather than swapping the root (call it node
-	 * A) with its successor, it's successor (call it Node B) is moved to the root
-	 * position by splaying for the deletion key in A's right subtree. Finally, A's
-	 * right child is made the new root's right child.
+	/**
+	 * Elimina un nodo al indicar su nombre
+	 * 		(Tambien de debe indicar la ubicacion en este caso)
+	 * @param clave
+	 * 		Nombre del nodo a eliminar 
+	 * 
+	 *( @param indx
+	 * 			ubicacion a eliminar)
 	 */
-	public void remove(String key) {
+	public void remove(String clave, Integer indx) {
 		if (root == null)
-			return; // empty tree
-
-		root = splay(root, key);
-
-		int cmp = key.compareTo(root.key);
-
+			return; 
+		root = splay(root, clave);
+		int cmp = clave.compareTo(root.clave);
+		
 		if (cmp == 0) {
 			if (root.left == null) {
-				root = root.right;
+				
+				if ((root.arrayIndx.getLength() == 1 &&  root.arrayIndx.getFirst().getDato().equals(indx))|| indx == -1) {
+					root = root.right;
+				}else {
+					root.remArrayIndx(indx);
+				}
+				
+			} else if (root.right == null) {
+				
+				if ((root.arrayIndx.getLength() == 1 &&  root.arrayIndx.getFirst().getDato().equals(indx))|| indx == -1) {
+					root = root.left;
+				}else {
+					root.remArrayIndx(indx);
+				}
+				
 			} else {
-				SplayNode x = root.right;
-				root = root.left;
-				splay(root, key);
-				root.right = x;
+				if ((root.arrayIndx.getLength() == 1 &&  root.arrayIndx.getFirst().getDato().equals(indx))|| indx == -1) {
+					SplayNode x = root.right;
+					root = root.left;
+					root = splay(root, clave);
+					root.right = x;
+				}else {
+					root.remArrayIndx(indx);
+				}
 			}
 		}
-
 		// else: it wasn't in the tree to remove
 	}
-
 	/***************************************************************************
 	 * Splay tree function.
 	 **********************************************************************/
@@ -133,14 +170,14 @@ public class SplayTree<String extends Comparable<String>, Value> {
 		if (h == null)
 			return null;
 
-		int cmp1 = key.compareTo(h.key);
+		int cmp1 = key.compareTo(h.clave);
 
 		if (cmp1 < 0) {
 			// key not in tree, so we're done
 			if (h.left == null) {
 				return h;
 			}
-			int cmp2 = key.compareTo(h.left.key);
+			int cmp2 = key.compareTo(h.left.clave);
 			if (cmp2 < 0) {
 				h.left.left = splay(h.left.left, key);
 				h = rotateRight(h);
@@ -162,7 +199,7 @@ public class SplayTree<String extends Comparable<String>, Value> {
 				return h;
 			}
 
-			int cmp2 = key.compareTo(h.right.key);
+			int cmp2 = key.compareTo(h.right.clave);
 			if (cmp2 < 0) {
 				h.right.left = splay(h.right.left, key);
 				if (h.right.left != null)
@@ -208,7 +245,13 @@ public class SplayTree<String extends Comparable<String>, Value> {
 			return 1 + size(x.left) + size(x.right);
 	}
 
-	// right rotate
+	/**
+	 * Realiza la rotacion a la derecha
+	 * @param h
+	 * 			Nodo a rotar
+	 * @return
+	 * 		Nodo aplicada la rotacion 
+	 */
 	private SplayNode rotateRight(SplayNode h) {
 		SplayNode x = h.left;
 		h.left = x.right;
@@ -216,7 +259,13 @@ public class SplayTree<String extends Comparable<String>, Value> {
 		return x;
 	}
 
-	// left rotate
+	/**
+	 * Realiza la rotacion a la izquierda
+	 * @param h 
+	 * 			Nodo a rotar
+	 * @return
+	 * 			Nodo aplicada la rotacion
+	 */
 	private SplayNode rotateLeft(SplayNode h) {
 		SplayNode x = h.right;
 		h.right = x.left;

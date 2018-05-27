9c18659ac98547674356ee158ca80b69cb543a43
ch99AM <christianalpizar99@gmail.com>
2018-05-27 03:44:41 -0600

    Arbol b completo, sirve la inserccion y la busquedad, maneja los indices de las canciones en listas en cada  nodo

--- OdysseyServer/src/com/odysseyserver/arboles/BTree.java
+++ OdysseyServer/src/com/odysseyserver/arboles/BTree.java
@@ -1,235 +1,293 @@
 package com.odysseyserver.arboles;
 
+import com.odysseyserver.listas.SimpleList;
+import com.odysseyserver.listas.SimpleNode;
 
+import sun.java2d.pipe.SpanShapeRenderer.Simple;
 
-public class BTree<T extends Comparable<T>>  {
-    // max children per B-tree node = M-1
-    // (must be even and greater than 2)
-    private static final int M = 4;
+/**
+ * Arbol B, Metodos get, insertar, ToString
+ * @author Christian
+ *
+ * @param <T>
+ */
+public class BTree<T extends Comparable<T>> {
+	private static final int M = 4;
 
-    private Node root;       // root of the B-tree
-    private int height;      // height of the B-tree
-    private int n;           // number of key-value pairs in the B-tree
+	private NodeB root;
+	private int height;
+	// private int n; //
 
-    
-    private static final class Node {
-        private int m;                             // number of children
-        private Entry[] children = new Entry[M];   // the array of children
-        // create a node with k children
-        private Node(int k) {
-            m = k;
-        }
-    }
+	private static final class NodeB {
+		private int numChildren;
+		private Entry[] children = new Entry[M];
 
-    // internal nodes: only use key and next
-    // external nodes: only use key and value
-    private static class Entry {
-        private Comparable key;
-        private final Object val;
-        private Node next;     // helper field to iterate over array entries
-        public Entry(Comparable key, Object val, Node next) {
-            this.key  = key;
-            this.val  = val;
-            this.next = next;
-        }
-    }
+		private NodeB(int k) {
+			numChildren = k;
+		}
+	}
 
-    /**
-     * Initializes an empty B-tree.
-     */
-    public BTree() {
-        root = new Node(0);
-    }
- 
-    /**
-     * Returns true if this symbol table is empty.
-     * @return {@code true} if this symbol table is empty; {@code false} otherwise
-     */
-    public boolean isEmpty() {
-        return size() == 0;
-    }
+	// internal nodes: only use key and next
+	// external nodes: only use key and value
+	private static class Entry<T> {
+		private T clave;
+		private NodeB next; // helper field to iterate over array entries
+		private SimpleList<Integer> arrayIndx;
 
-    /**
-     * Returns the number of key-value pairs in this symbol table.
-     * @return the number of key-value pairs in this symbol table
-     */
-    public int size() {
-        return n;
-    }
+		public Entry(T key, Integer indx, NodeB next) {
+			this.clave = key;
+			this.next = next;
+			arrayIndx = new SimpleList<>();
+			this.addArrayIndx(indx);
+		}
 
-    /**
-     * Returns the height of this B-tree (for debugging).
-     *
-     * @return the height of this B-tree
-     */
-    public int height() {
-        return height;
-    }
+		public Entry(T key, SimpleList<Integer> arrayIndx, NodeB next) {
+			this.clave = key;
+			this.next = next;
+			this.arrayIndx = arrayIndx;
+		}
 
+		/**
+		 * Elimina ubicaciones de la lista de indes
+		 * 
+		 * @param indx
+		 *            Ubicacion a eliminar
+		 */
+		public void remArrayIndx(Integer indx) {
+			arrayIndx.remove(indx);
+		}
 
-    /**
-     * Returns the value associated with the given key.
-     *
-     * @param  key the key
-     * @return the value associated with the given key if the key is in the symbol table
-     *         and {@code null} if the key is not in the symbol table
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
-    public T get(T key) {
-        if (key == null) throw new IllegalArgumentException("argument to get() is null");
-        return search(root, key, height);
-    }
+		/**
+		 * Anade ubicaciones a lista de indices
+		 * 
+		 * @param indx
+		 *            Ubicacion a anadir
+		 */
+		public void addArrayIndx(Integer indx) {
+			this.arrayIndx.add(new SimpleNode<Integer>(indx));
+		}
 
-    private T search(Node x, T key, int ht) {
-        Entry[] children = x.children;
+		public SimpleList<Integer> getArrayIndx() {
+			return this.arrayIndx;
+		}
+	}
 
-        // external node
-        if (ht == 0) {
-            for (int j = 0; j < x.m; j++) {
-                if (eq(key, children[j].key)) return (T) children[j].val;
-            }
-        }
+	/**
+	 * Inicializa el arbol B
+	 */
+	public BTree() {
+		root = new NodeB(0);
+	}
 
-        // internal node
-        else {
-            for (int j = 0; j < x.m; j++) {
-                if (j+1 == x.m || less(key, children[j+1].key))
-                    return search(children[j].next, key, ht-1);
-            }
-        }
-        return null;
-    }
+	/**
+	 * Verifica que arbol esta vacio root == 0
+	 * 
+	 * @return True o false
+	 */
+	public boolean isEmpty() {
+		return root == null;
+	}
 
+	// /**
+	// * @return Numero de claves que hay en el arbol
+	// */
+	// public int size() {
+	// return n;
+	// }
 
-    /**
-     * Inserts the key-value pair into the symbol table, overwriting the old value
-     * with the new value if the key is already in the symbol table.
-     * If the value is {@code null}, this effectively deletes the key from the symbol table.
-     *
-     * @param  key the key
-     * @param  val the value
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
-    public void put(T key) {
-        if (key == null) throw new IllegalArgumentException("argument key to put() is null");
-        Node u = insert(root, key, height); 
-        n++;
-        if (u == null) return;
+	/**
+	 * Retorna la altura del arbol
+	 *
+	 * @return Altura del arbol B
+	 */
+	public int height() {
+		return height;
+	}
 
-        // need to split root
-        Node t = new Node(2);
-        t.children[0] = new Entry(root.children[0].key, null, root);
-        t.children[1] = new Entry(u.children[0].key, null, u);
-        root = t;
-        height++;
-    }
+	/**
+	 * Retorna los indices de una clave, si esta se encuentra en el arbol
+	 * 
+	 * @param clave
+	 *            the key
+	 * @return Array de indices
+	 */
+	public SimpleList<Integer> get(T clave) {
+		if (clave == null)
+			throw new IllegalArgumentException("argument to get() is null");
+		return search(root, clave, height);
+	}
 
-    private Node insert(Node nuevo,T key, int ht) {
-        int j;
-        Entry t = new Entry(key, key, null);
+	private SimpleList<Integer> search(NodeB current, T key, int ht) {
+		Entry[] children = current.children;
+		// external node
+		if (ht == 0) {
+			for (int j = 0; j < current.numChildren; j++) {
+				if (eq(key, (T) children[j].clave))
+					return children[j].arrayIndx;
+			}
+		}
+		// internal node
+		else {
+			for (int j = 0; j < current.numChildren; j++) {
+				if (j + 1 == current.numChildren || less(key, (T) children[j + 1].clave))
+					return search(children[j].next, key, ht - 1);
+			}
+		}
+		return null;
+	}
 
-        // external node
-        if (ht == 0) {
-            for (j = 0; j < nuevo.m; j++) {
-                if (less(key, nuevo.children[j].key)) break;
-            }
-        }
-        // internal node
-        else {
-            for (j = 0; j < nuevo.m; j++) {
-                if ((j+1 == nuevo.m) || less(key, nuevo.children[j+1].key)) {
-                    Node u = insert(nuevo.children[j++].next, key, ht-1);
-                    if (u == null) return null;
-                    t.key = u.children[0].key;
-                    t.next = u;
-                    break;
-                }
-            }
-        }
-        for (int i = nuevo.m; i > j; i--)
-            nuevo.children[i] = nuevo.children[i-1];
-        nuevo.children[j] = t;
-        nuevo.m++;
-        if (nuevo.m < M) return null;
-        else         return split(nuevo);
-    }
+	/**
+	 * Inserta un nuevo dato, crea el nodo nuevo si ya no hay espacio en el nodo en
+	 * el cual le corresponde ir
+	 *
+	 * @param clave
+	 *            Clave del nodo
+	 */
 
-    // split node in half
-    private Node split(Node h) {
-        Node t = new Node(M/2);
-        h.m = M/2;
-        for (int j = 0; j < M/2; j++)
-            t.children[j] = h.children[M/2+j]; 
-        return t;    
-    }
+	public void insert(T clave, Integer indx) {
+		if (clave == null) {
+			throw new IllegalArgumentException("Clave nula");
+		}
+		NodeB nuevo = insert(root, clave, indx, height);
+		if (nuevo == null) {
+			return;
+		}
+		NodeB t = new NodeB(2);
+		t.children[0] = new Entry((T) root.children[0].clave, root.children[0].arrayIndx, root);
+		t.children[1] = new Entry((T) nuevo.children[0].clave, root.children[0].arrayIndx, nuevo);
+		root = t;
+		height++;
+	}
 
-    /**
-     * Returns a string representation of this B-tree (for debugging).
-     *
-     * @return a string representation of this B-tree.
-     */
-    public String toString() {
-        return toString(root, height, "") + "\n";
-    }
+	private NodeB insert(NodeB current, T key, Integer indx, int ht) {
+		int j;
+		Entry t = new Entry(key, indx, null);
+		if (ht == 0) {
+			for (j = 0; j < current.numChildren; j++) {
+				if (less(key, (T) current.children[j].clave))
+					break;
+				else if (eq(key, (T) current.children[j].clave)) {
+					current.children[j].addArrayIndx(indx);
+					return null;
+				}
+			}
+		} else {
+			for (j = 0; j < current.numChildren; j++) {
+				if ((j + 1 == current.numChildren) || less(key, (T) current.children[j + 1].clave)) {
+					NodeB u = insert(current.children[j++].next, key, indx, ht - 1);// POSIBLE PROBLEMA
+					if (u == null)
+						return null;
+					t.clave = u.children[0].clave;
+					t.arrayIndx = u.children[0].arrayIndx;
+					t.next = u;
+					break;
+				} else if ((j + 1 == current.numChildren) || eq(key, (T) current.children[j].clave)) {
+					current.children[j].addArrayIndx(indx);
+					return null;
+				}
+			}
+		}
+		for (int i = current.numChildren; i > j; i--)
+			current.children[i] = current.children[i - 1];
+		current.children[j] = t;
+		current.numChildren++;
+		if (current.numChildren < M)
+			return null;
+		else
+			return split(current);
+	}
 
-    private String toString(Node h, int ht, String indent) {
-        StringBuilder s = new StringBuilder();
-        Entry[] children = h.children;
+	// split node in half
+	private NodeB split(NodeB h) {
+		NodeB t = new NodeB(M / 2);
+		h.numChildren = M / 2;
+		for (int j = 0; j < M / 2; j++)
+			t.children[j] = h.children[M / 2 + j];
+		return t;
+	}
 
-        if (ht == 0) {
-            for (int j = 0; j < h.m; j++) {
-                s.append(indent + children[j].key + " " + children[j].val + "\n");
-            }
-        }
-        else {
-            for (int j = 0; j < h.m; j++) {
-                if (j > 0) s.append(indent + "(" + children[j].key + ")\n");
-                s.append(toString(children[j].next, ht-1, indent + "     "));
-            }
-        }
-        return s.toString();
-    }
+	/**
+	 * Returns a string representation of this B-tree (for debugging).
+	 *
+	 * @return a string representation of this B-tree.
+	 */
+	public String toString() {
+		return toString(root, height, "") + "\n";
+	}
 
+	private String toString(NodeB h, int ht, String indent) {
+		StringBuilder s = new StringBuilder();
+		Entry[] children = h.children;
 
-    // comparison functions - make Comparable instead of Key to avoid casts
-    private boolean less(Comparable k1, Comparable k2) {
-        return k1.compareTo(k2) < 0;
-    }
+		if (ht == 0) {
+			for (int j = 0; j < h.numChildren; j++) {
+				s.append(indent + children[j].clave + printIndx(children[j].getArrayIndx()) + "\n");
+			}
+		} else {
+			for (int j = 0; j < h.numChildren; j++) {
+				if (j > 0)
+					s.append(indent + "(" + children[j].clave + printIndx(children[j].getArrayIndx()) + ")\n");
+				s.append(toString(children[j].next, ht - 1, indent + "     "));
+			}
+		}
+		return s.toString();
+	}
 
-    private boolean eq(Comparable k1, Comparable k2) {
-        return k1.compareTo(k2) == 0;
-    }
+	// Aux para imprimir lista de indices
+	private String printIndx(SimpleList<Integer> indx) {
+		String strIndx = "";
+		System.out.println(indx.getLength());
+		for (int i = 0; i < indx.getLength(); i++) {
+			strIndx += " " + indx.find(i);
+		}
+		return strIndx;
+	}
 
+	// comparison functions - make Comparable instead of Key to avoid casts
+	private boolean less(T k1, T k2) {
+		return k1.compareTo(k2) < 0;
+	}
 
-    /**
-     * Unit tests the {@code BTree} data type.
-     *
-     * @param args the command-line arguments
-     */
-    public static void main(String[] args) {
-    	
-        BTree<String> st = new BTree<String>();
+	private boolean eq(T k1, T k2) {
+		return k1.compareTo(k2) == 0;
+	}
 
-        st.put("www.cs.princeton.edu");
-        st.put("www.cs.princeton.edu");
-        st.put("www.princeton.edu");
-        st.put("www.yale.edu");
-        st.put("www.simpsons.com");
-        st.put("www.apple.com");
-        st.put("www.amazon.com");
-        st.put("www.ebay.com");
-        st.put("www.cnn.com");
-        st.put("www.google.com");
-        st.put("www.nytimes.com");
-        st.put("www.microsoft.com");
-        st.put("www.dell.com");
-        st.put("www.slashdot.org");
-        st.put("www.espn.com");
-        st.put("www.weather.com");
-        st.put("www.yahoo.com");
-        System.out.println(st.toString());
+	/**
+	 * Unit tests the {@code BTree} data type.
+	 *
+	 * @param args
+	 *            the command-line arguments
+	 */
+	public static void main(String[] args) {
 
-    }
+		BTree<String> st = new BTree<String>();
+		st.insert("g", 85);
+		st.insert("p", 41);
+		st.insert("a", 96);
+		st.insert("k", 52);
+		st.insert("c", 63);
+		st.insert("h", 94);
+		st.insert("q", 21);
+		st.insert("t", 78);
+		st.insert("l", 32);
+		st.insert("g", 85);
+		st.insert("p", 41);
+		st.insert("a", 96);
+		st.insert("k", 52);
+		st.insert("c", 63);
+		st.insert("h", 94);
+		st.insert("q", 21);
+		st.insert("t", 78);
+		st.insert("l", 32);
+		System.out.println(st.get("q"));
+		System.out.println(st.get("y"));
+		System.out.println(st.get("k"));
+		System.out.println(st.get("g"));
+		System.out.println(st.get("h"));
+		System.out.println(st.get("g"));
+		System.out.println(st.get("t"));
+		System.out.println(st.toString());
+
+	}
 
 }
-
